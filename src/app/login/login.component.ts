eb6f00138efbea4b992776b36aafda59ee9aad8c
kristina_cein <kristina.nesterova@hotmail.co.uk>
2020-10-24 22:42:53 +0300

    Initial commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import 'firebase/auth';
+import { AngularFireAuth } from '@angular/fire/auth';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  constructor(private fb: FormBuilder, private auth: AngularFireAuth, private router: Router) { }
 
   ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+    });
   }
 
+  onLogin(): void {
+  const {email, password} = this.loginForm.value;
+  this.auth.signInWithEmailAndPassword(email, password)
+    .then(() => this.router.navigate(['']));
+  }
 }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -2,8 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
+import { from } from 'rxjs';
+import {switchMap, first, mapTo, take} from 'rxjs/operators';
+import 'firebase/auth';
+import * as firebase from 'firebase';
 
-
+class CreateUserGQL {
+}
 
 @Component({
   selector: 'app-signup',
@@ -12,6 +17,7 @@ import { Router } from '@angular/router';
 })
 export class SignupComponent implements OnInit {
   registerForm: FormGroup;
+  private createUserGql: CreateUserGQL;
   constructor(private fb: FormBuilder, private auth: AngularFireAuth, private router: Router) { }
 
   ngOnInit(): void {
@@ -20,11 +26,27 @@ export class SignupComponent implements OnInit {
       password: new FormControl('', [Validators.required, Validators.minLength(4)]),
     });
   }
-  createAccount(): void{
-    const {email, password} = this.registerForm.value;
-    this.auth.createUserWithEmailAndPassword(email, password).then(user => {
-      console.log('RegisterComponent -> createUser -> user', user);
-      this.router.navigate(['']);
-    });
+  createUser() {
+    const { email, password, fullName } = this.registerForm.value;
+    from(this.auth.createUserWithEmailAndPassword(email, password))
+      .pipe(
+        switchMap(({ user }) => this.metadataCreateWatcher(user)),
+        take(1),
+        switchMap((user) => from(user.getIdToken(true)).pipe(mapTo(user))),
+        switchMap(({ uid: uuid }) =>
+          this.createUserGql.mutate({ uuid, fullName })
+        )
+      )
+      .subscribe(() => this.router.navigate(['']), console.error);
+  }
+
+  private metadataCreateWatcher(user: firebase.User) {
+    return this.fb
+      .object(`metadata/${user.uid}/refreshTime`)
+      .valueChanges()
+      .pipe(
+        first((refreshTime) => !!refreshTime),
+        mapTo(user)
+      );
   }
 }
