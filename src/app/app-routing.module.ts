1bf75a78b002e7e8f08a0c080f92d4024a632258
kristina_cein <kristina.nesterova@hotmail.co.uk>
2020-10-24 22:06:14 +0300

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,31 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {Routes, RouterModule, ActivatedRouteSnapshot, RouterStateSnapshot} from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { HomeComponent } from './home/home.component';
-import { AngularFireAuthGuard } from '@angular/fire/auth-guard';
+import { redirectLoggedInTo, redirectUnauthorizedTo} from '@angular/fire/auth-guard';
+import { canActivate } from '@angular/fire/auth-guard';
+import { pipe } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['login']);
+const redirectToHome = redirectLoggedInTo(['']);
+const redirectToHomeWithLogger = (
+  next: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot ) => pipe(
+    tap (() => console.info('will be redirected')),
+  redirectToHome);
 
-const routes: Routes = [{ path: 'login', component: LoginComponent},
+const routes: Routes = [{ path: 'login', component: LoginComponent,
+  ...canActivate(redirectToHomeWithLogger)},
                         { path: 'signup', component: SignupComponent},
                         { path: 'home', component: HomeComponent },
   {
     path: '',
     component: HomeComponent,
-    canActivate: [AngularFireAuthGuard]
+    ...canActivate(redirectUnauthorizedToLogin),
   },
 ];
-
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
