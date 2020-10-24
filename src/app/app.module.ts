a3abb20d0b8e7a03ee9299996335e97f9b62f945
kristina_cein <kristina.nesterova@hotmail.co.uk>
2020-10-24 12:16:33 +0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import {RouterModule} from '@angular/router';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { FormsModule } from '@angular/forms';
+import { SignupComponent } from './signup/signup.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDFnCog7Zfx71bJTEQmTrBICxWLfviWTU0',
+  authDomain: 'react-test-563e6.firebaseapp.com',
+  databaseURL: 'https://react-test-563e6.firebaseio.com',
+  projectId: 'react-test-563e6',
+  storageBucket: 'react-test-563e6.appspot.com',
+  messagingSenderId: '843469197110',
+  appId: '1:843469197110:web:5cf84376a6ca3f4f60ad97'
+};
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    SignupComponent,
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
